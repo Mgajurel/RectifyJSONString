67e214a55c08be3184ad6a494a2a8d2e28858f4f
Kushal Gajurel <kushalgajurel@Kushals-MacBook-Pro.local>
2019-02-26 01:18:45 +0545

    Structure for class

---
+++ RectifyJsonString.py
@@ -0,0 +1,22 @@
+import json
+
+
+class RectifyJSONString(object):
+    '''Doc String for RectifyJSONString
+
+    This is the main class for the task in hand of rectifying
+    before providing it to json.loads
+
+    Args:
+        object (default): default python object
+    '''
+
+    def __init__(self):
+        '''INIT
+
+        Initialization for RectifyJSONString
+        '''
+        pass
+
+    def rectify_string(self, string):
+        pass
